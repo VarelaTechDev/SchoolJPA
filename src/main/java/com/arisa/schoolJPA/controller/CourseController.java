d76eff840447b67124e15c8d3b0e5d671c1b6117
ArisaBonsaiTree <angelevarela@hotmail.com>
2023-03-15 10:38:21 -0700

    Implemented find by id

--- src/main/java/com/arisa/schoolJPA/controller/CourseController.java
+++ src/main/java/com/arisa/schoolJPA/controller/CourseController.java
@@ -5,10 +5,7 @@ import com.arisa.schoolJPA.entity.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,5 +27,13 @@ public class CourseController {
     public ResponseEntity<String> helloWorld() {
         return ResponseEntity.status(HttpStatus.OK).body("Hello World");
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> getCourseById(@PathVariable("id") Integer id){
+        Course course = courseRepo.findById(id).orElse(null);
+        return new ResponseEntity<Course>(course, HttpStatus.OK);
+    }
+
+
 
 }
